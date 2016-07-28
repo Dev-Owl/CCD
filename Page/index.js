952c723c355b121da060ea00171affad0476349a
Christian Muhle <christian.muhle@spectec.net>
2016-07-28 22:37:03 +0200

    Added bootsrap and other libs + related css Example page for first screen Exmaple ajax call and test of notify plugin

--- Page/index.js
+++ Page/index.js
@@ -1,6 +1,9 @@
 var express = require('express'),
-    app = express();
+	app = express();
  
 app.use(express.static('public'));
+app.post('/groups', function(req, res){
+	res.send("It works");
+});
  
 app.listen(8080);
